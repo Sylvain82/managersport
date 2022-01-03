ced9a7b155b63c4a31a8bac468868fbbbdfaa19b
Sylvain82 <76108593+Sylvain82@users.noreply.github.com>
2022-01-03 18:48:22 +0100

    mailjet inscription

--- src/Classe/Mail.php
+++ src/Classe/Mail.php
@@ -2,40 +2,46 @@
 
 namespace App\Classe;
 
-
 use Mailjet\Client;
 use Mailjet\Resources;
 
-class Mail {
 
+class Mail
+{
     private $api_key = 'cb5a20dba4fa0c5f965eaa6f03d3d844';
-    private $api_key_secret = 'f9aca67a882aa2140e56b0078bba5f01';
+    private $api_key_secret = '1fbe117783ff2b3a9407c743a838ab05';
 
-    public function send($to_email,$to_name,$subject,$content) {
-        $mj = new Client( $this->api_key, $this->api_key_secret, true, ["version"=>'v3.1'] );
+    public function send($to_email, $to_name, $subject, $content)
+    {
+
+   $mj = new Client($this->api_key, $this->api_key_secret, true, ['version' => 'v3.1']);
+
         $body = [
-            'Messages' => [
-                [
-                    'From' => [
-                        'Email' => "sylvain.toulousain@hotmail.fr",
-                        'Name' => "Football Statistics"
-                    ],
-                    'To' => [
-                        [
-                            'Email' => "$to_email",
-                            'Name' => "$to_name"
+                'Messages' => [
+                    [
+                        'From' => [
+                            'Email' => "sylvain.toulousain@hotmail.fr",
+                            'Name' => "La boutique française"
+                        ],
+                        'To' => [
+                            [
+                                'Email' => $to_email,
+                                'Name' => $to_name
+                            ]
+                        ],
+                        'TemplateID' => 2676606,
+                        'TemplateLanguage' => true,
+                        'Subject' => $subject,
+                        'variables' => [
+                            'content' => $content,
                         ]
-                    ],
-                    'TemplateID' => 2859035,
-                    'TemplateLanguage' => true,
-                    'Subject' => "$subject",
-                    'variables'=>[
-                        'content'=> $content,
                     ]
                 ]
-            ]
-        ];
+            ];
         $response = $mj->post(Resources::$Email, ['body' => $body]);
-        $response->success() && ($response->getData());
+
+        $response->success();
+
     }
 }
+
