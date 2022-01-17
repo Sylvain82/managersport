e86cd347a749bb2e1efa92c8243d5fed85fde46d
Sylvain82 <76108593+Sylvain82@users.noreply.github.com>
2022-01-17 16:41:23 +0100

    style dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -24,14 +24,13 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Managersport');
+            ->setTitle('Tableau de bord');
     }
 
     public function configureMenuItems(): iterable
     {
 
         yield MenuItem::linkToRoute('Retour site','fas fa-arrow-left', 'home');
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('User', 'fas fa-users', User::class);
         yield MenuItem::linkToCrud('Players', 'fas fa-running', Player::class);
         yield MenuItem::linkToCrud('Equipes', 'fas fa-user-friends', Equipe::class);
